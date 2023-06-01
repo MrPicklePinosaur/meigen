31acc8c923914cdcbd9dfc1be8cdca8d0de3ec68
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-06-01 20:31:06 +0900

    'add intro to parsing files'

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import System.Environment
+import Text.Parsec
+import Text.Parsec.String
+import Control.Monad
+
+import Parser (myParser)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    a <- getArgs
+    case a of
+      [str] -> either print print $ parse myParser "" str
+      _ -> error "please pass one argument with the string to parse"
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,15 @@
 module Parser where
 
+import Text.Parsec (ParseError)
+import Text.Parsec.String (Parser)
+import Text.Parsec.String.Parsec (try)
+import Text.Parsec.String.Char (oneOf, char, digit, satisfy)
+import Text.Parsec.String.Combinator (many1, choice, chainl1)
+import Control.Applicative ((<|>), many)
+import Control.Monad (void)
+import Data.Char (isLetter, isDigit)
+import FunctionsAndTypesForParsing
+import Text.Parsec.Char (string)
 
+myParser :: Parser ()
+myParser = void $ string "hello"
