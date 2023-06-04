0a72eefd4b5081649d617f772dfb70540b72050d
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-06-04 19:21:33 +0900

    simplify expressions using applicative operators

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,9 +3,9 @@ module Parser where
 import Text.Parsec (ParseError)
 import Text.Parsec.String (Parser)
 import Text.Parsec.String.Parsec (try)
-import Text.Parsec.String.Char (oneOf, char, digit, satisfy)
+import Text.Parsec.String.Char (oneOf, char, digit, letter, satisfy)
 import Text.Parsec.String.Combinator (many1, choice, chainl1)
-import Control.Applicative ((<|>), many)
+import Control.Applicative ((<$>), (<*>), (<*), (*>), (<|>), many, (<$))
 import Control.Monad (void)
 import Data.Char (isLetter, isDigit)
 import FunctionsAndTypesForParsing
@@ -13,76 +13,57 @@ import Text.Parsec.Char (string)
 
 myParser = pExpr
 
--- Always consume trailing whitespace
-lexeme :: Parser a -> Parser a
-lexeme p = do
-    x <- p
-    whitespace
-    return x
-
--- Consume leading whitespace
-parseWithWhitespace :: Parser a -> String -> Either ParseError a
-parseWithWhitespace p = parseWithEof wrapper
-    where
-        wrapper = do
-            whitespace
-            p
-
 data Expr = Num Integer
     | Var String
     | Add Expr Expr
     | Paren Expr
     deriving (Show, Eq)
 
-pExpr = do
-    t <- pTerm
-    maybeAddSuffix t
-        where
-            addSuffix t0 = do
-                void $ lexeme $ char '+'
-                t1 <- pTerm
-                maybeAddSuffix (Add t0 t1)
-            -- attempt to add suffix but return original if it fails (backtrack)
-            maybeAddSuffix t0 = addSuffix t0 <|> return t0
+-- Always consume trailing whitespace
+lexeme :: Parser a -> Parser a
+lexeme p = p <* whitespace
 
+-- Consume leading whitespace
+parseWithWhitespace :: Parser a -> String -> Either ParseError a
+parseWithWhitespace p = parseWithEof $ whitespace *> p
+
+pExpr :: Parser Expr
+pExpr = chainl1 pTerm (Add <$ pChar '+')
 
 pTerm :: Parser Expr
-pTerm = try pNum <|> pVar <|> pParen
+pTerm = pNum <|> pVar <|> pParen pExpr
 
 -- Parse Num
 pNum :: Parser Expr
-pNum = do
-    n <- lexeme $ many1 digit
-    return $ Num $ read n
+pNum = Num <$> pInt
+
+pInt :: Parser Integer
+pInt = read <$> lexeme (many1 digit)
 
 -- Parse Var
 pVar :: Parser Expr
-pVar = lexeme $ do
-    x <- firstChar
-    xs <- many nonFirstChar
-    return $ Var (x:xs)
+pVar = Var <$> pIden
+
+pIden :: Parser String
+pIden = lexeme ((:) <$> firstChar <*> many nonFirstChar)
     where
-        firstChar = satisfy (\c -> isLetter c || c == '_')
-        nonFirstChar = satisfy (\c -> isLetter c || isDigit c || c == '_')
+        firstChar = letter <|> char '_'
+        nonFirstChar = letter <|> digit <|> char '_'
 
 -- Parse Paren
-pParen :: Parser Expr
-pParen = do
-    void $ lexeme $ char '('
-    contents <- pExpr
-    void $ lexeme $ char ')'
-    return (Paren $ contents)
+pParen :: Parser Expr -> Parser Expr
+pParen inner = Paren <$> (pChar '(' *> inner <* pChar ')')
+
+pChar :: Char -> Parser Char
+pChar = lexeme <$> char
 
 -- Parse Add
 pAdd :: Parser Expr
-pAdd = do
-    -- TOOD currently right assoicative
-    l <- pTerm
-    void $ lexeme $ char '+'
-    r <- pExpr
-    return $ Add l r
+pAdd = Add <$> l <*> r
+    where
+        l = pTerm <* pChar '+'
+        r = pExpr
 
 whitespace :: Parser ()
-whitespace = do
-    void $ many $ oneOf " \n\t"
+whitespace = void $ many $ oneOf " \n\t"
 
