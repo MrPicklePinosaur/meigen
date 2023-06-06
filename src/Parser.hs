58b518a8a7c98fa57cd66d3d3f9b1dcc58a9bb35
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-06-06 22:04:17 +0900

    add hspec

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,18 +4,20 @@ import Text.Parsec (ParseError)
 import Text.Parsec.String (Parser)
 import Text.Parsec.String.Parsec (try)
 import Text.Parsec.String.Char (oneOf, char, digit, letter, satisfy)
-import Text.Parsec.String.Combinator (many1, choice, chainl1, between)
+import Text.Parsec.String.Combinator (many1, choice, chainl1, between, notFollowedBy)
 import Control.Applicative ((<$>), (<*>), (<*), (*>), (<|>), many, (<$))
-import Control.Monad (void)
+import Control.Monad (void, guard)
 import Data.Char (isLetter, isDigit)
 import FunctionsAndTypesForParsing
 import Text.Parsec.Char (string)
+
+import qualified Test.Hspec as H
 
 import qualified Text.Parsec.String.Expr as E
 
 myParser = pExpr
 
-data BinOp = Add | Sub | Mult | Div
+data BinOp = Add | Sub | Mult | Div | Less | LessEq | Great | GreatEq | Eq
     deriving (Show, Eq)
 
 data UnOp = Pos | Neg
@@ -29,19 +31,23 @@ data Expr = Num Integer
     deriving (Show, Eq)
 
 exprTable :: [[E.Operator Expr]]
-exprTable = [unOp Pos '+'
-            ,unOp Neg '-'
-            ,binOp Mult '*' E.AssocLeft
-            ,binOp Div '/' E.AssocLeft
-            ,binOp Add '+' E.AssocLeft
-            ,binOp Sub '-' E.AssocLeft
+exprTable = [unOp Pos "+"
+            ,unOp Neg "-"
+            ,binOp LessEq "<=" E.AssocLeft
+            ,binOp Less "<" E.AssocLeft
+            ,binOp GreatEq ">=" E.AssocLeft
+            ,binOp Great ">" E.AssocLeft
+            ,binOp Mult "*" E.AssocLeft
+            ,binOp Div "/" E.AssocLeft
+            ,binOp Add "+" E.AssocLeft
+            ,binOp Sub "-" E.AssocLeft
              ]
+    where
+        binOp :: BinOp -> String -> E.Assoc -> [E.Operator Expr]
+        binOp op sym assoc = [E.Infix (Binary op <$ pSym sym) assoc]
 
-binOp :: BinOp -> Char -> E.Assoc -> [E.Operator Expr]
-binOp op ch assoc = [E.Infix (Binary op <$ pChar ch) assoc]
-
-unOp :: UnOp -> Char -> [E.Operator Expr]
-unOp op ch = [E.Prefix (Unary op <$ pChar ch)]
+        unOp :: UnOp -> String -> [E.Operator Expr]
+        unOp op sym = [E.Prefix (Unary op <$ pSym sym)]
 
 pExpr :: Parser Expr
 pExpr = E.buildExpressionParser exprTable pTerm
@@ -81,6 +87,21 @@ pParen inner = Paren <$> between (pChar '(') (pChar ')') inner
 pChar :: Char -> Parser Char
 pChar = lexeme <$> char
 
+pSym :: String -> Parser String
+pSym s = try $ lexeme $ do
+    void $ string s
+    -- TODO should dynamically generate list of characters that make up symbols?
+    notFollowedBy (oneOf "<>=+-^%/*")
+    return s
+
 whitespace :: Parser ()
 whitespace = void $ many $ oneOf " \n\t"
+
+-- tests
+
+-- operatorTests :: [(String, Expr)]
+-- operatorTests = [("1 < 2", (Binary Less (Num 1) (Num 2)))]
+
+-- runTests :: (Eq a, Show a) => Parser a -> (String, a) -> H.Test
+-- runTests parser (input, expected) = H.TestLabel input $ H.TestCase $ do
 
