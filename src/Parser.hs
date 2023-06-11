e21ead4eb00b4e4c333b3a1bf69e0d8cdff4da55
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-06-11 18:07:37 +0900

    more bases

--- src/Parser.hs
+++ src/Parser.hs
@@ -17,41 +17,57 @@ import qualified Data.Map as M
 import qualified Text.Parsec.String.Expr as E
 import Text.Parsec (optionMaybe)
 import Data.Maybe (fromMaybe)
+import Text.Parsec (option)
 
 data Literal = Num Integer
 
 -- pNum :: String -> Parser Integer
 -- pNum =
 
-myParser = kDigitTen
+-- myParser = kDigit
+myParser = pNum
+
+pNum :: Parser Integer
+pNum = kDigitHundred
+
+-- Takes in a 'base' character, 'base' value as well as the next parser to use
+kDigitBuilder :: Char -> Integer -> Parser Integer -> Parser Integer
+kDigitBuilder ch base next = try include <|> skip
+    where
+        include = do
+            left <- option 1 kDigit
+            void $ pChar ch
+            right <- next
+            return $ left * base + right
+        skip = next
+
+kDigitHundred :: Parser Integer
+kDigitHundred = kDigitBuilder '百' 100 kDigitTen
 
 kDigitTen :: Parser Integer
-kDigitTen = do
-    left <- optionMaybe kDigit
-    void $ pChar '十'
-    right <- optionMaybe kDigit
-    return $ (fromMaybe 1 left) * 10 + (fromMaybe 0 right)
+kDigitTen = kDigitBuilder '十' 10 kDigitMaybe
 
+kDigitMaybe :: Parser Integer
+kDigitMaybe = try kDigit <|> return 0
+
+-- Return zero if not matched
 kDigit :: Parser Integer
 kDigit = choice $ map _kDigit _digitLookup
-
-_kDigit :: (Char, Integer) -> Parser Integer
-_kDigit (c, val) = satisfy (==c) >> return val
-
--- kDigit = oneOf "一二三四五六七八九" 
-
-_digitLookup :: [(Char, Integer)]
-_digitLookup = [
-    ('一', 1),
-    ('二', 2),
-    ('三', 3),
-    ('四', 4),
-    ('五', 5),
-    ('六', 6),
-    ('七', 7),
-    ('八', 8),
-    ('九', 9)
-  ]
+    where
+        _kDigit :: (Char, Integer) -> Parser Integer
+        _kDigit (c, val) = satisfy (==c) >> return val
+        _digitLookup :: [(Char, Integer)]
+        _digitLookup = [
+            ('一', 1),
+            ('二', 2),
+            ('三', 3),
+            ('四', 4),
+            ('五', 5),
+            ('六', 6),
+            ('七', 7),
+            ('八', 8),
+            ('九', 9)
+          ]
 
 whitespace :: Parser ()
 whitespace = void $ many $ oneOf " \n\t"
